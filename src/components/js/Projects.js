91aef24514537ff07ba7ea7b833ac2c306c46d31
NICK JAMES <32378305+nbjames2@users.noreply.github.com>
2020-06-19 22:23:45 -0400

    added music notifier screenshot

--- src/components/js/Projects.js
+++ src/components/js/Projects.js
@@ -7,7 +7,7 @@ export default (props) => {
     <section className='projects-container'>
       <h2 className='title'>Projects</h2>
       <div className='projects-box-wrapper'>
-        <Project title='Music Notifier' description='Built using Node, EJS, and PostreSQL' image='/logo192.png' link='http://music.bairdjames.com/' />
+        <Project title='Music Notifier' description='Built using Node, EJS, and PostreSQL' image='/musicNotifierScreenShot.png' link='http://music.bairdjames.com/' />
         <Project title='Webstore' description='Build using React, React Router, Node, and MongoDB' image='/logo192.png' link='http://store.bairdjames.com/' />
         <Project title='Nick-Chat' description='Built using React, and Firebase DB and Auth' image='/nick-chat-screenshot.png' link='http://chat.bairdjames.com/' />
       </div>
