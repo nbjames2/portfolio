90044071a2d3c18a7246bbb5a093044cbe3d1f9e
NICK JAMES <32378305+nbjames2@users.noreply.github.com>
2020-06-21 16:01:51 -0400

    forgot to add new assets

---
+++ src/components/js/ProjectInfoDialog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function ProjectInfoDialog (props) {
+  return (
+    <div className='info-dialog-container'>
+      <div className='info-dialog-inner'>
+        <div className='info-dialog-inner-inner'>
+          <div className='project-title-row'>
+            <h3>{props.title}</h3>
+            <i className='material-icons-outlined' onClick={props.closeDialog}>close</i>
+          </div>
+          <p dangerouslySetInnerHTML={{ __html: props.info }} />
+        </div>
+      </div>
+    </div>
+  );
+}
